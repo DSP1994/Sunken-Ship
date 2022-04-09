0b2de6921fed8d2ddde92d1689618de834e99be0
David <parrydavid@live.co.uk>
2022-04-09 09:20:16 +0000

    add a function that displays the game to the user.

--- run.py
+++ run.py
@@ -52,3 +52,26 @@ def getRandomWord(wordList):
     # a function that returns a random word from the above list
     wordIndex = random,randinit(0, len(wordList) -1)
     return wordList[wordIndex]
+
+def diplayBoard(missingLetters, correctLetters, secretWord):
+    print(SUNKEN_SHIP_PICS[len(missedLetters)])
+    print()
+
+    print('Missed letters:', end=' ')
+    for letter in missedLetters:
+        print(letter, end=' ')
+    print()
+
+    blanks = '_' * len(secretWord)
+
+    for i in range(len(secretWord)):
+        # Replaces the blanks with correctly guessed letters
+        if secretWord[i] in correctLetters:
+            blanks = blanks[:i] + secretWord[i] + blanks[i+1:]
+
+    for letter in blanks:
+        #Shows the secret word with spaces between each letter
+        print(letter, end=' ')
+    print()
+
+displayBoard()
