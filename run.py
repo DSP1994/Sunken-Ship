d2ada3f8835a887b47abe2f4e65e40be84e2b1a9
David <parrydavid@live.co.uk>
2022-04-09 09:01:20 +0000

    Add different sea creature names used in the game, using split so not have to write a list, done automatically

--- run.py
+++ run.py
@@ -1,37 +1,39 @@
 import random
 from pprint import pprint
 SUNKEN_SHIP_PICS = [
+    # The capital letters indicate that this is
+    # not to be changed, under any circumstances.
 """
-      /|\ 
-     /_|_\ 
+      /|\
+     /_|_\
    ____|____
    \_o_o_o_/
 ~~   |       ~~~~~
      t
 """,
 """
-      /|\ 
-     /_|_\ 
+      /|\
+     /_|_\
    ____|____
    \_o_o_o_/
 ~~   |       ~~~~~
 """, 
 """
-      /|\ 
-     /_|_\ 
+      /|\
+     /_|_\
    ____|____
    \_o_o_o_/
 ~~~         ~~~~~~
 """, 
 """
-      /|\ 
-     /_|_\ 
+      /|\
+     /_|_\
    ____|____
 ~~~         ~~~~~~
 """, 
 """
-      /|\ 
-     /_|_\ 
+      /|\
+     /_|_\
 ~~~~~     ~~~~~~~~
 """, 
 """
@@ -40,4 +42,8 @@ SUNKEN_SHIP_PICS = [
 """, """
 ~~~~~~~~~~~~~~~~~~
 """]
-pprint(SUNKEN_SHIP_PICS)
+
+words = 'clam crab manatee turtle cuttlefish prawn \
+        sponge shrimp squid lobster mackerel seal hammerhead \
+        sealion marlin pufferfish octopus dolphin sunfish seahorse \
+        stringray eagleray whaleshark clownfish starfish toadfish'.split()
