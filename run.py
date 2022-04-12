f44a2f7ca29e12e58aad35bfeb6e519ba1351b0d
David <parrydavid@live.co.uk>
2022-04-12 10:47:21 +0000

    add finishing touches to program. correct indents, spellings etc.

--- run.py
+++ run.py
@@ -42,35 +42,31 @@ words = 'clam crab manatee turtle cuttlefish prawn\
         stringray eagleray whaleshark clownfish starfish toadfish'.split()
 
 
-def get_random_word(word_list):
+def get_random_word(list_of_words):
     # a function that returns a random word from the above list
-    word_index = random.randint(0, len(word_list) - 1)
-    return word_list[word_index]
+    letter_index = random.randint(0, len(list_of_words) - 1)
+    return list_of_words[letter_index]
 
 
-def display_board(missed_letters, correct_letters, secret_word):
-    print(SUNKEN_SHIP_PICS[len(missed_letters)])
+def game_overview(characters_omitted, characters_found, mystery_string):
+    print(SUNKEN_SHIP_PICS[len(characters_omitted)])
     print()
-
     print('Missed letters:', end=' ')
-    for letter in missed_letters:
+    for letter in characters_omitted:
+        print(letter, end=' ')
+    print()
+    blanks = '_' * len(mystery_string)
+    for i in range(len(mystery_string)): 
+        # Replace blanks with correctly guessed letters.
+        if mystery_string[i] in characters_found:
+            blanks = blanks[:i] + mystery_string[i] + blanks[i+1:]
+    for letter in blanks: 
+        # Show the secret word with spaces in between each letter.
         print(letter, end=' ')
     print()
 
-    blanks = '_' * len(secret_word)
 
-    for i in range(len(secret_word)):
-        # Replaces the blanks with correctly guessed letters
-        if secret_word[i] in correct_letters:
-            blanks = blanks[:i] + secret_word[i] + blanks[i+1:]
-
-    for letter in blanks:
-        # Shows the secret word with spaces between each letter
-        print(letter, end=' ')
-    print()
-
-
-def get_guess(already_guessed):
+def recieve_player_guess(previously_recieved):
     # Returns the letter the player guessed. Also ensures
     # the player enters a single letter, nothing else.
     while True:
@@ -78,8 +74,8 @@ def get_guess(already_guessed):
         guess = input()
         guess = guess.lower()  # this will turn even an uppercase into lower
         if len(guess) != 1:  # checks if one character long (ie a letter)
-            print('Please enter a single letter')
-        elif guess in already_guessed:
+            print('Please enter a single letter.')
+        elif guess in previously_recieved:
             print('You have already guessed that letter. Choose again.')
         elif guess not in 'abcdefghijklmnopqrstuvwxyz':
             print('Please enter a LETTER.')
@@ -87,64 +83,68 @@ def get_guess(already_guessed):
             return guess
 
 
-def play_again():
+def do_you_want_to_play_again():
     # Function that returns True if player wants to play again.
     # otherwise returns as false.
-    print('Do you want to go again? (yes or no)')
+    print('Do you want to play again? (yes or no)')
     return input().lower().startswith('y')
 
 
 # Is the title of the game
-print('S U N K E N S H I P')
+print('S U N K E N   S H I P')
+
 # will be blank until the player has guessed some letters.
-missed_letters = ''
+characters_omitted = ''
+
 # same as above.
-correct_letters = ''
+characters_found = ''
+
 # won't be displayed to the user
-secret_word = get_random_word(words)
+mystery_string = get_random_word(words)
+
 # will only be set to true when it's been given a signal by the user
 # that they want to play again.
-game_is_done = False
+is_sunken_ship_finished = False
+
 
 # calls the displayBoard function, giving it three variables
 while True:
-    display_board(missed_letters, correct_letters, secret_word)
+    game_overview(characters_omitted, characters_found, mystery_string)
     # lets the player enter a letter
-    guess = get_guess(missed_letters + correct_letters)
-    if guess in secret_word:
-        correct_letters = correct_letters + guess
+    guess = recieve_player_guess(characters_omitted + characters_found)
+    if guess in mystery_string:
+        characters_found = characters_found + guess
 
         # checking if the player has won, needs to check every letter
         # in every position to ensure win. ie 1 letter could be in 2
         # different locations.
-        found_all_letters = True
-        for i in range(len(secret_word)):
-            if secret_word[i] not in correct_letters:
-                found_all_letters = False
+        all_characters_guessed = True
+        for i in range(len(mystery_string)):
+            if mystery_string[i] not in characters_found:
+                all_characters_guessed = False
                 break
-            if found_all_letters:
-                print('Yes!! The secret word was "' +
-                      secret_word + '"! You won!')
-                game_is_done = True
+        if all_characters_guessed:
+            print('Yes! The secret word is "' 
+            + mystery_string + '"! You have won!')
+            is_sunken_ship_finished = True
+    else:
+        characters_omitted = characters_omitted + guess
+        # check if the player has guessed too many times and lost.
+        if len(characters_omitted) == len(SUNKEN_SHIP_PICS) - 1:
+            game_overview(characters_omitted, characters_found, mystery_string)
+            print('You have run out of guesses!\nAfter '
+              + str(len(characters_omitted)) + ' missed guesses and '
+              + str(len(characters_found)) + ' correct guesses, the word was "'
+              + mystery_string + '"')
+            is_sunken_ship_finished = True
+    
+    # Asking the player if they wish to try again.
+    # (but only if the game is done).
+    if is_sunken_ship_finished:
+        if do_you_want_to_play_again():
+            characters_omitted = ''
+            characters_found = ''
+            is_sunken_ship_finished = False
+            mystery_string = get_random_word(words)
         else:
-            missed_letters = missed_letters + guess
-
-            # check if the player has guessed too many times and lost.
-            if len(missed_letters) == len(SUNKEN_SHIP_PICS) - 1:
-                display_board(missed_letters, correct_letters, secret_word)
-                print('You have run out of guesses!\nAfter ' +
-                      str(len(missed_letters)) + ' missed guesses and ' +
-                      str(len(correct_letters)) + ' correct guesses, \
-                            the word was "' + secret_word + '"')
-                game_is_done = True
-
-            # Asking the player if they wish to try again.
-            # (but only if the game is done).
-            if game_is_done:
-                if play_again():
-                    missed_letters = ''
-                    correct_letters = ''
-                    game_is_done = False
-                    secret_word = get_random_word(words)
-                else:
-                    break
+            break
